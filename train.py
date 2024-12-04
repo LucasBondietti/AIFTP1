b77ac9b3a51a57c263b5fa5b49ad2c5fd2c5427a
LucasBondietti <lucasbondietti@gmail.com>
2024-12-04 21:57:13 +0100

    training NN on MNIST Dataset script

---
+++ train.py
@@ -0,0 +1,44 @@
+import argparse
+from statistics import mean
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from tqdm import tqdm
+
+from model import MNISTNet
+
+# setting device on GPU if available, else CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def train(net, optimizer, loader, epochs=10):
+    criterion = nn.CrossEntropyLoss()
+    for epoch in range(epochs):
+        running_loss = []
+        t = tqdm(loader)
+        for x, y in t:
+            x, y = x.to(device), y.to(device)
+            outputs = net(x)
+            loss = criterion(outputs, y)
+            running_loss.append(loss.item())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            t.set_description(f'training loss: {mean(running_loss)}')
+
+def test(model, dataloader):
+    test_corrects = 0
+    total = 0
+    with torch.no_grad():
+        for x, y in dataloader:
+            x = x.to(device)
+            y = y.to(device)
+            y_hat = model(x).argmax(1)
+            test_corrects += y_hat.eq(y).sum().item()
+            total += y.size(0)
+    return test_corrects / total
+
+
