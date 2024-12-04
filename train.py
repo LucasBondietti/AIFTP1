4499f0e5b9fd9ca6ad1a11eef819bd983e4f61b3
LucasBondietti <lucasbondietti@gmail.com>
2024-12-04 22:03:26 +0100

    Dataloaders instantiation

--- train.py
+++ train.py
@@ -54,3 +54,16 @@ if __name__=='__main__':
 	batch_size = args.batch_size
 	nb_epochs = args.nb_epochs
 	lr = args.lr
+     
+    # transforms
+	transform = transforms.Compose(
+		[transforms.ToTensor(),
+		transforms.Normalize((0.5,), (0.5,))])
+ 
+	# datasets
+	trainset = torchvision.datasets.MNIST('../data', download=True, train=True, transform=transform)
+	testset = torchvision.datasets.MNIST('../data', download=True, train=False, transform=transform)
+ 
+	# dataloaders
+	trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+	testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
